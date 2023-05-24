273dd8b74a6196ff33e9124fe3a78df279cd2427
rl627 <rl627@drexel.edu>
2023-05-24 09:43:17 -0400

    ETL: simualted spatial ids and relationships

---
+++ .etl/code/simulation/zcta-county-place.R
@@ -0,0 +1,37 @@
+
+# Define a function to generate spatial data
+generate_spatial_data <- function(state, n = 1000) {
+  
+  set.seed(123)
+  
+  data <- data.frame(
+    State = rep(state, n),
+    ZCTA = sample(sprintf("%05d",1:99999), n, replace = TRUE),
+    County_Code = sample(sprintf("%03d",1:999), n, replace = TRUE),
+    City_Name = sample(c("Everwood Springs", "Silverstone Ridge", "Coppermill Valley", "Liberty Meadows", 
+                         "Fox Hollow", "Iron Gate", "Redcliff Hills", "Stony Creek", "Pinebrook Heights", 
+                         "Lakeview Haven", "Willow Glen", "Oakdale Village", "Diamond Peak", 
+                         "Sunrise Bluff", "Shadowbrook"), n, replace = TRUE)
+  )
+ 
+  final =  data %>% 
+    clean_names() %>% 
+    as_tibble() 
+  
+  return(final)
+}
+
+# Generate spatial data for PA and NY
+PA_spatial_data <- generate_spatial_data("PA")
+NY_spatial_data <- generate_spatial_data("NY")
+
+# Combine all the data
+final_spatial_data = rbind(PA_spatial_data, NY_spatial_data) %>% 
+  mutate(state_fip = ifelse(state=='PA','11','12'),
+         county = paste0(state_fip, county_code)) %>% 
+  rename(city = city_name) %>% 
+  select(zcta, county, city, state)
+
+# Write the final data to a csv file
+final_spatial_data %>% write.csv("clean/spatial_relationships.csv", row.names = FALSE)
+final_spatial_data %>% write_parquet("clean/spatial_relationships.parquet")
--- .etl/index.R
+++ .etl/index.R
@@ -10,13 +10,16 @@
   library(janitor)
   library(stringr)
 
+  ## Imports
+  xwalk_spatial = read_parquet("clean/spatial_relationships.parquet")
 }
 
 
 { # Simulate data -----------------------------------------------------------
 
-  
+  "code/simulation/zcta-county-place.R"
   "code/simulation/hcup.R"
+  "code/simulation/pa_death.R"
   
 }
 
