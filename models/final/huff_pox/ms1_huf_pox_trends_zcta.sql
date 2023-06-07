347edd2b4b8c4970bf6388643d43db4de5c31344
rl627 <rl627@drexel.edu>
2023-06-07 12:16:57 -0400

    added state and year to intermedaite and final huf pox models #11

--- models/final/huff_pox/ms1_huf_pox_trends_zcta.sql
+++ models/final/huff_pox/ms1_huf_pox_trends_zcta.sql
@@ -10,7 +10,14 @@ huf_pox_hosp AS (
     FROM {{ ref('int__huf_pox__hosp_zcta_count') }}
 )
 
-SELECT *
+SELECT 
+    huf_pox_hosp.zcta, 
+    huf_pox_hosp.state, 
+    huf_pox_hosp.year, 
+    n_huf_pox_hosp,
+    n_huf_pox_death
 FROM huf_pox_hosp
 JOIN huf_pox_deaths
 ON huf_pox_hosp.zcta = huf_pox_deaths.zcta
+    AND huf_pox_hosp.state = huf_pox_deaths.state
+    AND huf_pox_hosp.year = huf_pox_deaths.year
--- models/intermediate/huf_pox/int__huf_pox__death_zcta_count.sql
+++ models/intermediate/huf_pox/int__huf_pox__death_zcta_count.sql
@@ -1,7 +1,10 @@
 {{ config(materialized='external', format =  target.schema) }}
 
 select 
-   zcta, COUNT(*) AS n_huf_pox_death
+   zcta, 
+   year,
+   state,
+   COUNT(*) AS n_huf_pox_death
 from {{ref('int__huf_pox__mortality')}}
 where flag_huff_pox = 'T'
-group by zcta
+group by zcta, year, state
