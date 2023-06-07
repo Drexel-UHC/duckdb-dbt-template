6aeb6ca4426181cc0c1e80c837eff388f315ffb1
rl627 <rl627@drexel.edu>
2023-06-07 11:20:29 -0400

    create intermedaite hospitalization model #11 #10

---
+++ models/intermediate/int__hospitalization.sql
@@ -0,0 +1,32 @@
+{{ config(materialized='external', format =  target.schema) }}
+with 
+
+cte_1 as (
+  select 
+    *,
+    from 
+      (
+      select * from {{ref('HOSPITALIZATION_NY_2010')}}
+      union
+      select * from {{ref('HOSPITALIZATION_NY_2015')}}
+      union
+      select * from {{ref('HOSPITALIZATION_PA_2010')}}
+      union
+      select * from {{ref('HOSPITALIZATION_PA_2015')}}
+    )
+)
+
+
+ 
+SELECT 
+  patient_id, 
+  state,
+  year,
+  zcta,
+  age, 
+  gender,
+  diagnosis_code AS dx_code, 
+  total_charges,
+  dataset
+FROM cte_1
+
