4e32bfe4402ae48396d7cb582b9fee44cddd7386
Matt Jannetti <maj353@drexel.edu>
2023-06-07 17:02:36 -0400

    replace inner join with full join #10

--- models/intermediate/huf_pox/int__huf_pox__hosp_death_zcta_year_counts.sql
+++ models/intermediate/huf_pox/int__huf_pox__hosp_death_zcta_year_counts.sql
@@ -2,4 +2,4 @@
 
 SELECT DISTINCT m.zcta, m.year, m.state, m.n_huf_pox_death, h.n_huf_pox_hosp
 FROM {{ref('int__huf_pox__death_zcta_count')}} m
-JOIN {{ref('int__huf_pox__hosp_zcta_count')}} h ON h.zcta=m.zcta AND h.year=h.year
+FULL JOIN {{ref('int__huf_pox__hosp_zcta_count')}} h ON h.zcta=m.zcta AND h.year=h.year
